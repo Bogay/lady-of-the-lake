e0e3f31b802973cf48780cff56297419dbee2b54
bogay <pojay11523@gmail.com>
2022-08-27 21:28:08 +0800

    feat: setup game data

--- lady-of-the-lake/godot/script/show-character.gd
+++ lady-of-the-lake/godot/script/show-character.gd
@@ -4,16 +4,20 @@ extends TextureRect
 # Should be `PlayerSelection`
 export(Resource) var player_selection
 
-onready var collisionShape: CollisionShape2D = $Area2D/CollisionShape2D
+onready var collision_shape: CollisionShape2D = $Area2D/CollisionShape2D
 
 func _ready():
 	assert(player_selection != null)
 	assert(player_selection is PlayerSelection)
-	assert(collisionShape != null)
+	assert(collision_shape != null)
 
-func _on_resized():
-	if collisionShape == null:
-		return
-	print('resized')
-	var shape: RectangleShape2D = collisionShape.shape
-	shape.extents = rect_size
+func _process(_delta):
+	sync_collision_shape()
+
+func sync_collision_shape():
+	var shape = collision_shape.shape as RectangleShape2D
+	assert(shape != null)
+	var x = rect_size.x / 2
+	var y = rect_size.y / 2
+	shape.extents = Vector2(x, y)
+	collision_shape.position = Vector2(x, y)
