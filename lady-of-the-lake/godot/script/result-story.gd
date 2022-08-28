6c7e165b68bdbb5f9fa77255bc501319677530a7
bogay <pojay11523@gmail.com>
2022-08-29 01:24:14 +0800

    feat: play rate scene

--- lady-of-the-lake/godot/script/result-story.gd
+++ lady-of-the-lake/godot/script/result-story.gd
@@ -2,6 +2,7 @@ class_name ResultStory
 extends Control
 
 export(Resource) var item
+export(Resource) var expected_item
 
 onready var item_renderer: TextureRect = $Item
 onready var description_label: Label = $Description/MarginContainer/Label
@@ -13,3 +14,13 @@ func _ready():
 	assert(_item != null)
 	item_renderer.texture = _item.texture
 	description_label.text = description_label.text.replace('__', _item.keyword)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("left_click"):
+		if item == expected_item:
+			var success_scene = preload('res://scenes/rate/success.tscn')
+			get_tree().get_root().add_child(success_scene.instance())
+		else:
+			var success_scene = preload('res://scenes/rate/fail.tscn')
+			get_tree().get_root().add_child(success_scene.instance())
+		self.queue_free()
