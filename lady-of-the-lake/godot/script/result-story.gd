f12abf5853780d1c50ca4d6a4b9c2f57ed898fe7
bogay <pojay11523@gmail.com>
2022-08-28 22:39:31 +0800

    feat: add result scenes

---
+++ lady-of-the-lake/godot/script/result-story.gd
@@ -0,0 +1,14 @@
+extends Control
+
+export(Resource) var item
+
+onready var item_renderer: TextureRect = $Item
+onready var description_label: Label = $Description/MarginContainer/Label
+
+var _item: Item
+
+func _ready():
+	_item = item
+	assert(_item != null)
+	item_renderer.texture = _item.texture
+	description_label.text = description_label.text.replace('__', _item.keyword)
